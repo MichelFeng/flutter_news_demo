5fecfa58a06245facb24c1e2e56a1ae054df2975
fengzhongshuang <fengzhongshuang@bytedance.com>
2021-10-09 16:24:39 +0800

    feat: welcome
    Change-Id: Icaff5126c00f70662b7943933d409df7369d16b8

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'pages/welcome/welcome.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: WelcomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
